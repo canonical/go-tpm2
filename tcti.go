2f80ccd9eaef7fb028b5de69f96d0c136fe2c285
Chris Coulson <chris.coulson@canonical.com>
2023-01-25 23:27:01 +0000

    documentation clarifications

--- tcti.go
+++ tcti.go
@@ -34,10 +34,14 @@ type TCTI interface {
 	// Read is used to receive a response to a previously transmitted command. The implementation
 	// must support partial reading of a response, and must return io.EOF when there are no more
 	// bytes of a response left to read.
+	//
+	// Reads can block and should consider the previously configured timeout. Once a response
+	// has been received from the device and when part of the response is read from this interface,
+	// subsequent reads to obtain the rest of the response should not block.
 	Read(p []byte) (int, error)
 
-	// Write is used to transmit a serialized command to the TPM implementation. A command must be
-	// transmitted in a single write.
+	// Write is used to transmit a serialized command to the TPM implementation. Commands are
+	// written in a single write. Writes should be non blocking.
 	Write(p []byte) (int, error)
 
 	Close() error
