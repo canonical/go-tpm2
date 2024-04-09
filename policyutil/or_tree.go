b22bec549839dd3dbb861075c23ff37c4f334d8a
Chris Coulson <chris.coulson@canonical.com>
2024-04-09 21:21:59 +0100

    policyutil: split up some files

---
+++ policyutil/or_tree.go
@@ -0,0 +1,117 @@
+// Copyright 2023 Canonical Ltd.
+// Licensed under the LGPLv3 with static-linking exception.
+// See LICENCE file for details.
+
+package policyutil
+
+import (
+	"errors"
+
+	"github.com/canonical/go-tpm2"
+)
+
+const (
+	// policyOrMaxDigests sets a reasonable limit on the maximum number of or
+	// digests.
+	policyOrMaxDigests = 4096 // equivalent to a depth of 4
+)
+
+// ensureSufficientORDigests turns a single digest in to a pair of identical digests.
+// This is because TPM2_PolicyOR assertions require more than one digest. This avoids
+// having a separate policy sequence when there is only a single digest, without having
+// to store duplicate digests on disk.
+func ensureSufficientORDigests(digests tpm2.DigestList) tpm2.DigestList {
+	if len(digests) == 1 {
+		return tpm2.DigestList{digests[0], digests[0]}
+	}
+	return digests
+}
+
+type policyOrNode struct {
+	parent  *policyOrNode
+	digests tpm2.DigestList
+}
+
+type policyOrTree struct {
+	alg       tpm2.HashAlgorithmId
+	leafNodes []*policyOrNode
+}
+
+func newPolicyOrTree(alg tpm2.HashAlgorithmId, digests tpm2.DigestList) (out *policyOrTree, err error) {
+	if len(digests) == 0 {
+		return nil, errors.New("no digests")
+	}
+	if len(digests) > policyOrMaxDigests {
+		return nil, errors.New("too many digests")
+	}
+
+	var prev []*policyOrNode
+
+	for len(prev) != 1 {
+		// The outer loop runs on each level of the tree. If
+		// len(prev) == 1, then we have produced the root node
+		// and the loop should not continue.
+
+		var current []*policyOrNode
+		var nextDigests tpm2.DigestList
+
+		for len(digests) > 0 {
+			// The inner loop runs on each sibling node within a level.
+
+			n := len(digests)
+			if n > 8 {
+				// The TPM only supports 8 conditions in TPM2_PolicyOR.
+				n = 8
+			}
+
+			// Create a new node with the next n digests and save it.
+			node := &policyOrNode{digests: ensureSufficientORDigests(digests[:n])}
+			current = append(current, node)
+
+			// Consume the next n digests to fit in to this node and produce a single digest
+			// that will go in to the parent node.
+			trial := newComputePolicySession(alg, nil, true)
+			trial.PolicyOR(node.digests)
+			nextDigest, err := trial.PolicyGetDigest()
+			if err != nil {
+				return nil, err
+			}
+			nextDigests = append(nextDigests, nextDigest)
+
+			// We've consumed n digests, so adjust the slice to point to the next ones to consume to
+			// produce a sibling node.
+			digests = digests[n:]
+		}
+
+		// There are no digests left to produce sibling nodes.
+		// Link child nodes to parents.
+		for i, child := range prev {
+			child.parent = current[i>>3]
+		}
+
+		// Grab the digests for the nodes we've just produced to create the parent nodes.
+		prev = current
+		digests = nextDigests
+
+		if out == nil {
+			// Save the leaf nodes to return.
+			out = &policyOrTree{
+				alg:       alg,
+				leafNodes: current,
+			}
+		}
+	}
+
+	return out, nil
+}
+
+func (t *policyOrTree) selectBranch(i int) (out []tpm2.DigestList) {
+	node := t.leafNodes[i>>3]
+
+	for node != nil {
+		out = append(out, node.digests)
+		node = node.parent
+	}
+
+	return out
+}
